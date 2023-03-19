5b864781e55fe7ad954f28563e8bbd54e9d9c58b
wukongxzero <pavankushal15729@gmail.com>
2023-03-19 21:31:27 +0530

    constvariable

---
+++ constvariable.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+
+
+int main()
+{
+    const double celcius_freezing_temperature = 0.0;
+    const int max_character_life = 100;
+
+
+    int a;
+    a = 2;
+    a = 4;
+    std::cout << celcius_freezing_temperature << std::endl;
+    return 0;
+
+}
